a75366cfc8bb696e2076fd2171bfb377a913eec1
Lars Ole Simonsen <los@sirenia.eu>
2018-01-31 21:11:14 +0100

    Fix minor issue

--- InitMe.elm
+++ InitMe.elm
@@ -58,7 +58,7 @@ viewInputs =
     div []
         [ div [] [ text "Working directory:", input [ type_ "text" ] [] ]
         , div [] [ text "input1", input [ type_ "text" ] [] ]
-        , div [] [ text "input1", button [] [ text "Update" ] ]
+        , div [] [ button [] [ text "Update" ] ]
         ]
 
 
